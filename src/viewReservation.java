328dcd2bae6cce0e944091dd09792df397c48612
neph0isme <farisdanialrzi@gmail.com>
2025-02-05 23:13:32 +0800

    ada tukar sat

--- src/viewReservation.java
+++ src/viewReservation.java
@@ -1,11 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class viewReservation extends JFrame{
-	public JFrame Frame;
-	public JPanel Panel;
+	public JFrame frame;
+	public JPanel mainPanel;
+	public CardLayout cardLayout;
+    public DefaultTableModel tableModelins;
 	
-	
-	
+	public viewReservation() {
+		frame = new JFrame("View Reservation");
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        mainPanel = new JPanel(new CardLayout());
+	}
+	
+	
+}
+
+public static void main(String[] args) {
+	new viewReservation();
 }
