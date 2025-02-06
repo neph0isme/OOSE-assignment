78c92fdba88dc926e6f8890b467436e9c79f1ac2
neph0isme <farisdanialrzi@gmail.com>
2025-02-07 02:49:17 +0800

    added editReservation

--- src/editReservation.java
+++ src/editReservation.java
@@ -2,6 +2,15 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-class editReservation extends JFrame{
-
+public class editReservation extends JFrame{
+	public editReservation() {
+		setTitle("View Reservation");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setSize(600, 300);
+	    setLocationRelativeTo(null); // Centers the window on the screen
+	    getContentPane().setLayout(null);
+	}
+	
+	
+	
 }
--- src/rateReserve.java
+++ src/rateReserve.java
@@ -6,52 +6,57 @@ public class rateReserve extends JFrame {
     public rateReserve() {
         setTitle("Reservation Rating");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 400); // Set the size of the window
+        setSize(220, 400); // Set the size of the window
         setLocationRelativeTo(null); // Center the window on the screen
         getContentPane().setLayout(null);
         
         Panel panel = new Panel();
         panel.setBackground(new Color(0, 128, 255));
-        panel.setBounds(0, 0, 584, 76);
+        panel.setBounds(0, 0, 211, 76);
         getContentPane().add(panel);
         panel.setLayout(null);
         
         JLabel lblNewLabel = new JLabel("Rate and Review");
         lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        lblNewLabel.setBounds(10, 0, 564, 76);
+        lblNewLabel.setBounds(10, 0, 191, 76);
         panel.add(lblNewLabel);
         lblNewLabel.setForeground(Color.WHITE);
         lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
         
         JLabel lblNewLabel_1 = new JLabel("Rate from 1 to 5 Star");
         lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel_1.setBounds(23, 123, 114, 23);
+        lblNewLabel_1.setBounds(6, 148, 192, 38);
         getContentPane().add(lblNewLabel_1);
         
         JRadioButton rdbtnOne = new JRadioButton("1 STAR");
         rdbtnOne.setHorizontalAlignment(SwingConstants.CENTER);
-        rdbtnOne.setBounds(23, 153, 109, 23);
+        rdbtnOne.setBounds(6, 193, 192, 23);
         getContentPane().add(rdbtnOne);
         
         JRadioButton rdbtnTwo = new JRadioButton("2 STAR");
         rdbtnTwo.setHorizontalAlignment(SwingConstants.CENTER);
-        rdbtnTwo.setBounds(23, 179, 109, 23);
+        rdbtnTwo.setBounds(6, 219, 192, 23);
         getContentPane().add(rdbtnTwo);
         
         JRadioButton rdbtnThree = new JRadioButton("3 STAR");
         rdbtnThree.setHorizontalAlignment(SwingConstants.CENTER);
-        rdbtnThree.setBounds(23, 205, 109, 23);
+        rdbtnThree.setBounds(6, 245, 192, 23);
         getContentPane().add(rdbtnThree);
         
         JRadioButton rdbtnFour = new JRadioButton("4 STAR");
         rdbtnFour.setHorizontalAlignment(SwingConstants.CENTER);
-        rdbtnFour.setBounds(23, 231, 109, 23);
+        rdbtnFour.setBounds(6, 271, 192, 23);
         getContentPane().add(rdbtnFour);
         
         JRadioButton rdbtnFive = new JRadioButton("5 STAR");
         rdbtnFive.setHorizontalAlignment(SwingConstants.CENTER);
-        rdbtnFive.setBounds(23, 257, 109, 23);
+        rdbtnFive.setBounds(6, 297, 192, 23);
         getContentPane().add(rdbtnFive);
+        
+        JLabel lblRestaurantName = new JLabel("New label");
+        lblRestaurantName.setHorizontalAlignment(SwingConstants.CENTER);
+        lblRestaurantName.setBounds(6, 105, 192, 14);
+        getContentPane().add(lblRestaurantName);
     }
     
     @Override
