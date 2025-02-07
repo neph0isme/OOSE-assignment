ffcf9bdaedd58fb345c5e75410e818a844423ec4
kamaaahehe <a202169@siswa.ukm.edu.my>
2025-02-07 22:04:38 +0800

    user

--- src/UserLoginAs.java
+++ src/UserLoginAs.java
@@ -14,9 +14,7 @@ public class UserLoginAs extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -30,7 +28,6 @@ public class UserLoginAs extends JFrame {
 		});
 	}
 
-	
 	public UserLoginAs() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 480);
