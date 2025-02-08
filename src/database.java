90e88ee227bc5af62a73212aab4ee373dcb2ab99
kamaaahehe <a202169@siswa.ukm.edu.my>
2025-02-08 18:27:07 +0800

    put some sample data (login info)

--- src/database.java
+++ src/database.java
@@ -1,14 +1,47 @@
 import java.util.ArrayList;
 
-public class Database {
-	private ArrayList<Customer> customerList = new ArrayList<Customer>();
-	private ArrayList<Staff> staffList = new ArrayList<Staff>();
-	private ArrayList<Restaurant> restaurantList = new ArrayList<Restaurant>();
+
+public class database {
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
+    private ArrayList<RestaurantStaff> staffList = new ArrayList<RestaurantStaff>();
+    //static ArrayList<Restaurant> restaurants = new ArrayList<>();
+    //static ArrayList<Reservation> reservations = new ArrayList<>();
+
+    
+    // Pre-Stored Restaurant Login info:
+    RestaurantStaff staff1 = new RestaurantStaff("R001", "rest123");
+    RestaurantStaff staff2 = new RestaurantStaff("R002", "rest456");
+    Customer c1 = new Customer("liana02","121212");
+    Customer c2 = new Customer("zulanep","110404");
+    
+    /*Restaurant info:
+    (nama rest, type of cuisine, location, numofpax)*/
+    
+    /*Cust info:
+     (nama, no phone, email)*/
+    
+    /*reservation 
+     (reservationid, restaurantid, reservepax, custname, nophone, email, status)*/
+    
+    /*reservation (pov staff)
+    (reservationid, reservepax, custname, nophone, email, status)*/
+    
+    
+    
+    
+    
+    
+    
+    public database() { //constructor
+    
+    //add to arraylist 
+    staffList.add(staff1);
+	staffList.add(staff2);
 	
-	public Database() {
-		//Customer
-		Customer customer = new Customer("A101234","Arman","arman101@gmail.com","0123456789"," @rman");
-		
-		
-	}
+	customers.add(c1);
+	customers.add(c2);
+	
+    }
+
+    
 }
