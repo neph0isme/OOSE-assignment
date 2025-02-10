2c36ac52d526584946b12e1a55c9ae656c0f3338
kamaaahehe <a202169@siswa.ukm.edu.my>
2025-02-11 01:18:39 +0800

    update db ikit je

--- src/Staff.java
+++ src/Staff.java
@@ -2,17 +2,17 @@
 public class Staff extends User {
 	private String staffID;
 	
-	public Staff(String staffID, String name, String email, String phoneNumber, String password) {
-		super(name, email, phoneNumber, password);
+	public Staff(String staffID, String name, String email, String password) {
+		super(staffID, name, email, password);
 		
 		this.staffID = staffID;
 	}
 	
-	public String getMatricNumber() {
+	public String getStaffID() {
 		return staffID;
 	}
 	
-	public void setMatricNumber(String newStaffID) {
+	public void setStaffID(String newStaffID) {
 		this.staffID = newStaffID;
 	}
 	
--- src/database.java
+++ src/database.java
@@ -3,16 +3,20 @@ import java.util.ArrayList;
 
 public class database {
     private ArrayList<Customer> customers = new ArrayList<Customer>();
-    private ArrayList<RestaurantStaff> staffList = new ArrayList<RestaurantStaff>();
+    private ArrayList<Staff> staffList = new ArrayList<Staff>();
     //static ArrayList<Restaurant> restaurants = new ArrayList<>();
     //static ArrayList<Reservation> reservations = new ArrayList<>();
 
     
     // Pre-Stored Restaurant Login info:
-    RestaurantStaff staff1 = new RestaurantStaff("R001", "rest123");
-    RestaurantStaff staff2 = new RestaurantStaff("R002", "rest456");
-    Customer c1 = new Customer("liana02","121212");
-    Customer c2 = new Customer("zulanep","110404");
+    
+    //String staffID, String name, String email, String password
+    Staff staff1 = new Staff("R001", "Amiezah", "amie@gmail.com", "rest123");
+    Staff staff2 = new Staff("R002", "Harris", "harris@gmail.com", "rest456");
+    
+    //String custID, String name, String email, String phoneNumber, String password
+    Customer c1 = new Customer("liana02", "Liana Johari", "liana@gmail.com", "0122234466", "121212" );
+    Customer c2 = new Customer("zulanep", "Zulhanif Ramli", "zul@gmail.com", "0165523433", "110404");
     
     /*Restaurant info:
     (nama rest, type of cuisine, location, numofpax)*/
@@ -34,7 +38,7 @@ public class database {
     
     public database() { //constructor
     
-    //add to arraylist 
+    //add to array
     staffList.add(staff1);
 	staffList.add(staff2);
 	
