a93924c56d2a568bb11042613b94f62c25a1fccf
kamaaahehe <a202169@siswa.ukm.edu.my>
2025-02-11 22:36:21 +0800

    database for reservation & restaurant

---
+++ src/Reservation.java
@@ -0,0 +1,81 @@
+
+public class Reservation {
+	
+	private String reserveID;
+    private String restID;
+    private int reservePax;
+    private String custname;
+    private String custphone;
+    private String custemail;
+    private boolean reserveStatus;
+	
+	//(reservationid, restaurantid, reservepax, custname, nophone, email, status)
+	public Reservation(String reserveID, String restID, int reservePax, String custname, String custphone, String custemail, Boolean reserveStatus) {
+		
+		this.reserveID = reserveID;
+        this.restID = restID;
+        this.reservePax = reservePax;
+        this.custname = custname;
+        this.custphone = custphone;
+        this.custemail = custemail;
+        this.reserveStatus = reserveStatus;
+        
+	}
+	
+	public String getReserveID() {
+        return reserveID;
+    }
+
+    public void setReserveID(String reserveID) {
+        this.reserveID = reserveID;
+    }
+
+    public String getRestaurantID() {
+        return restID;
+    }
+
+    public void setRestaurantID(String restaurantID) {
+        this.restID = restID;
+    }
+
+    public int getReservePax() {
+        return reservePax;
+    }
+
+    public void setReservePax(int reservePax) {
+        this.reservePax = reservePax;
+    }
+
+    public String getCustName() {
+        return custname;
+    }
+
+    public void setCustName(String custName) {
+        this.custname = custname;
+    }
+
+    public String getCustPhone() {
+        return custphone;
+    }
+
+    public void setCustPhone(String custPhone) {
+        this.custphone = custphone;
+    }
+
+    public String getCustEmail() {
+        return custemail;
+    }
+
+    public void setCustEmail(String custEmail) {
+        this.custemail = custemail;
+    }
+
+    public boolean isReserveStatus() {
+        return reserveStatus;
+    }
+
+    public void setReserveStatus(boolean reserveStatus) {
+        this.reserveStatus = reserveStatus;
+    }
+
+}
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,14 +1,59 @@
 
 public class Restaurant {
-	private String RestaurantName;
+	private String restaurantName;
 	private String location;
 	private int restPax;
+	private double rating;
+	private String cuisine;
 
-	public Restaurant(String RestaurantName, String location, int restPax) {
-		this.RestaurantName = RestaurantName;
+	//(nama rest, type of cuisine, location, numofpax, rating)
+	public Restaurant(String restaurantName, String cuisine, String location, int restPax, double rating) {
+		this.restaurantName = restaurantName;
 		this.location = location;
 		this.restPax = restPax;
+		this.cuisine = cuisine;
+		this.rating = rating;
 	}
+	
+	public String getName() {
+        return restaurantName;
+    }
+
+    public void setName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getCuisineType() {
+        return cuisine;
+    }
+
+    public void setCuisineType(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getRestPax() {
+        return restPax;
+    }
+
+    public void setRestPax(int restPax) {
+        this.restPax = restPax;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 	
 	
 }
